f1db924837940c77c92205dea63260aeda27c122
HS H <3262398@naver.com>
2023-05-24 09:58:33 +0900

    firebase

--- react-firebase-store/src/App.js
+++ react-firebase-store/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
+import Home from './page/Home';
+import Login from './page/Login';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={}/>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
     </Routes>      
   );
 }
